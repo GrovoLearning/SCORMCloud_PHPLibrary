5a53cd7b0021b06494f302d5c2eea534bab4a7ec
Victoria Wu <vwu@grovo.com>
2018-07-31 10:30:42 -0700

    addStatusCreatd

--- src/CourseService.php
+++ src/CourseService.php
@@ -43,6 +43,7 @@ class CourseService{
 	
 	private $_configuration = null;
 
+	const CREATED = "created";
 	const RUNNING = "running";
 	const FINISHED = "finished";
 	const ERROR = "error";
@@ -411,6 +412,7 @@ class CourseService{
                 $xmlStatus = $this->getAsyncImportStatus($token);
                 $statusResult = (string) $xmlStatus->status;
                 switch ($statusResult) {
+                    case $this::CREATED:
                     case $this::RUNNING:
                         sleep(5);
                         break;
